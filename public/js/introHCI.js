72a26a47599baec2fbe16de8936ea6fb590f7b10
Jamie Lee <jamielee3136@gmail.com>
2016-02-11 06:27:04 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,19 +14,38 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
 	// Prevent following the link
-	e.preventDefault();
+	//e.preventDefault();
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
+	//console.log("The result " + result);
+
+}
+
+
+
+function addProject(result){
+
+	console.log(result);
+	//var projectHTML = '<a href="#" class="details">' + '<img src="' + result['image'] + '"class"="details">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p></a>';
+	var projectHTML = '<img src="' + result['image'] + '"class"="detailsImage">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p>' + '<p>' + result['summary'] + '</p>';
+	//console.log(projectHTML);
+
+	$("#project" + result['id'] + " .details").html(projectHTML);
+	
 }
 
 /*
@@ -35,4 +54,19 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColors);
 }
+
+function changeColors(result){
+	console.log(result);
+	var colors  = (result['colors'])['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+
